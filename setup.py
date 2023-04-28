14c4131a220cbb316f1e0079698b7d2924dc106e
joshjetson <joshjetson@gmail.com>
2023-04-28 13:08:38 -0500

    modified setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11"
+        "Programming Language :: Python :: 3.11",
         "Operating System :: OS Independent"
     ],
     packages=find_packages(),
